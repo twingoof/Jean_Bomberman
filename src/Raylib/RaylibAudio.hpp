9906c9004064dd5a041b18e998a3089365f871df
twingoof <prost.pierrick@outlook.fr>
2021-05-18 10:29:19 +0200

    Encapsulation-F: Adding end of wave/sound management encapsulation

--- src/Raylib/RaylibAudio.hpp
+++ src/Raylib/RaylibAudio.hpp
@@ -126,6 +126,113 @@ namespace Raylib {
              * @param sound Sound to play
              */
              extern "C" void playSound(Sound sound);
+
+             /**
+              * @brief Stop playing a sound
+              * @fn void stopSound(Sound sound);
+              * @param sound Sound to stop
+              */
+              extern "C" void stopSound(Sound sound);
+
+              /**
+               * @brief Pause a sound
+               * @fn void pauseSound(Sound sound);
+               * @param sound Sound to pause
+               */
+               extern "C" void pauseSound(Sound sound);
+
+               /**
+                * @brief Resume a paused sound
+                * @fn void resumeSound(Sound sound);
+                * @param sound Sound to resume
+                */
+                extern "C" void resumeSound(Sound sound);
+
+                /**
+                 * @brief Play a sound (using multichannel buffer pool)
+                 * @fn void playSoundMulti(Sound sound)
+                 * @param sound Sound to play
+                 */
+                 extern "C" void playSoundMulti(Sound sound);
+
+                 /**
+                  * @brief Stop any sound playing (using multichannel buffer poo)
+                  * @fn void stopSoundMulti()
+                  */
+                  extern "C" void stopSoundMulti();
+
+                  /**
+                   * @brief Get number of sounds playing in the multichannel
+                   * @fn int getSoundsPlaying()
+                   * @return The number of sounds playing in the multichannel
+                   */
+                   extern "C" int getSoundsPlaying();
+
+                   /**
+                    * @brief Check if the sound is playing
+                    * @fn bool isSoundPlaying(Sound sound)
+                    * @param sound Sound to check
+                    * @return \bTrue if the sound is playing, \bFalse otherwise
+                    */
+                    extern "C" bool isSoundPlaying(Sound sound);
+
+                    /**
+                     * @brief Set volume for a sound (1.0 is max level)
+                     * @fn void setSoundVolume(Sound sound, float volume)
+                     * @param sound Sound to set volume
+                     * @param volume Volume to set the sound
+                     */
+                     extern "C" void setSoundVolume(Sound sound, float volume);
+
+                     /**
+                      * @brief Set pitch for a sound (1.0 is base level)
+                      * @fn void setSoundPitch(Sound sound, float pitch)
+                      * @param sound Sound to set pitch
+                      * @param pitch Pitch to set the sound
+                      */
+                      extern "C" void setSoundPitch(Sound sound, float pitch);
+
+                      /**
+                       * @brief Convert wave data to desired format
+                       * @fn void waveFormat(Wave &wave, int sampleRate, int sampleSize, int channels)
+                       * @param wave Wave to convert
+                       * @param sampleRate Desired sample rate
+                       * @param sampleSize Desired sample size
+                       * @param channels Desired number of channels
+                       */
+                       extern "C" void waveFormat(Wave &wave, int sampleRate, int sampleSize, int channels);
+
+                       /**
+                        * @brief Copy a wave to new wave
+                        * @fn Wave waveCopy(Wave wave)
+                        * @param wave Wave to copy
+                        * @return A copy of the wave passed as parameter
+                        */
+                        extern "C" Wave waveCopy(Wave wave);
+
+                        /**
+                         * @brief Crop a wave to a defined sample range
+                         * @fn void waveCrop(Wave &wave, int initSample, int finalSample)
+                         * @param wave Wave to crop
+                         * @param initSample Start of the wave sample
+                         * @param finalSample End of the wave sample
+                         */
+                         extern "C" void waveCrop(Wave &wave, int initSample, int finalSample);
+
+                         /**
+                          * @brief Load sample data from wave as a float array
+                          * @fn std::vector<float> loadWaveSamples(Wave wave)
+                          * @param wave Wave to load data from
+                          * @return a float vector containing the wave's data
+                          */
+                          extern "C" std::vector<float> loadWaveSamples(Wave wave);
+
+                          /**
+                           * @brief Unload wave sample loaded with \code loadWaveSamples()
+                           * @fn void unloadWaveSamples(std::vector<float> samples)
+                           * @param samples Samples to unload
+                           */
+                           extern "C" void unloadWaveSamples(std::vector<float> samples);
 }
 
 #endif /* !RAYLIBAUDIO_HPP_ */
