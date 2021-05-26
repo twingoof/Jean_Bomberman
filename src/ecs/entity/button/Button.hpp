dbb4a491047f617cba400e5ee86186cbdb4adee0
Gildas Gonzalez <gildas.gonzalez@epitech.eu>
2021-05-26 17:11:02 +0200

    ECS-TF: Adding missing documentation

--- src/ecs/entity/button/Button.hpp
+++ src/ecs/entity/button/Button.hpp
@@ -5,19 +5,43 @@
 ** Button
 */
 
+/**
+ * @file Button.hpp
+ * @author gildas.gonzalez@epitech.eu; pierrick.prost@epitech.eu; pierrick.prost@epitech.eu; valentin.bouchet@epitech.eu; mathis.ragot@epitech.eu
+ * @brief File that contain the Button entity
+ * @version 0.1
+ * @date 2021-05-26
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef BUTTON_HPP_
 #define BUTTON_HPP_
 
 #include "IEntity.hpp"
 
+/**
+ * @class Button Button.hpp "src/ecs/entity/button/Button.hpp"
+ */
 class Button:public IEntity {
     public:
+        /**
+         * @fn Button()
+         * @brief Construct a new Button object
+         */
         Button();
-        ~Button();
-        void display() override;
 
-    protected:
-    private:
+        /**
+         * @fn ~Button()
+         * @brief Destroy the Button object
+         */
+        ~Button();
+
+        /**
+         * @fn void display() override
+         * @brief Display the button
+         */
+        void display() override;
 };
 
 #endif /* !BUTTON_HPP_ */
