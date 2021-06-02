b78b67d1d2994cb6ec8a541aa33c24c972064da6
Valentin Bouchet <valentin.bouchet@epitech.eu>
2021-06-03 01:03:44 +0200

    ECS-F: Adding Collectible component, and add Physics files component

---
+++ src/ecs/component/physics/Physics.cpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-NAN-4-1-indiestudio-gildas.gonzalez [WSL: Ubuntu-20.04]
+** File description:
+** Physics
+*/
+
+#include "Physics.hpp"
+
+Physics::Physics() = default;
+
+Physics::~Physics() = default;
