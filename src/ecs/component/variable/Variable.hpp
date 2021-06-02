45341cdb6b9919ec6f8171a28bb3c12e7c2abdf3
Gildas Gonzalez <gildas.gonzalez@epitech.eu>
2021-06-02 11:17:50 +0200

    ECS-TF: Adding vector3 component

--- src/ecs/component/variable/Variable.hpp
+++ src/ecs/component/variable/Variable.hpp
@@ -5,6 +5,16 @@
 ** Variable
 */
 
+/**
+ * @file Variable.hpp
+ * @author gildas.gonzalez@epitech.eu; pierrick.prost@epitech.eu; pierrick.prost@epitech.eu; valentin.bouchet@epitech.eu; mathis.ragot@epitech.eu
+ * @brief File that contain the Variable component
+ * @version 0.1
+ * @date 2021-06-02
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include <iostream>
 
 #ifndef VARIABLE_HPP_
@@ -12,18 +22,59 @@
 
 #include "IComponent.hpp"
 
+/**
+ * @class Variable Variable.hpp "src/ecs/component/variable/Variable.hpp"
+ */
 class Variable {
     public:
+        /**
+         * @fn Variable(int variable = 0)
+         * @brief Construct a new Variable object
+         * 
+         * @param variable 
+         */
         Variable(int variable = 0);
+
+        /**
+         * @fn ~Variable()
+         * @brief Destroy the Variable object
+         */
         ~Variable();
+        
+        /**
+         * @fn void add(int nb)
+         * @brief Add a number to the variable
+         * 
+         * @param nb Number to add
+         */
         void add(int nb);
+
+        /**
+         * @fn void setValue(int newVariable)
+         * @brief Sub a number to the variable
+         * 
+         * @param nb Number to sub
+         */
         void sub(int nb);
+
+        /**
+         * @fn void setValue(int newVariable)
+         * @brief Set the value of the variable
+         * 
+         * @param newVariable New value
+         */
         void setValue(int newVariable);
+
+        /**
+         * @fn const int getValue() const
+         * @brief Get the value of the variable
+         * 
+         * @return const int 
+         */
         const int getValue() const;
 
-
     private:
-        int _variable;
+        int _variable; /**< Variable*/
 };
 
 #endif /* !VARIABLE_HPP_ */
