7129004ad30a5b60bff4e13f4326bf5c402a3e19
Nom4D <arthur.adam@epitech.eu>
2021-06-04 15:23:30 +0200

    demo - F: Adding getter for window dimensions

---
+++ bootstrap/src/main.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-NAN-4-1-indiestudio-gildas.gonzalez
+** File description:
+** main
+*/
+
+#include <iostream>
+
+int main(void)
+{
+    return (std::printf("On a réalisé un cmake!"));
+}
