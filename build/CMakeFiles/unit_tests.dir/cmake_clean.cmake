a6ebc00e57d4713863b3dac7c80e60941504a463
Gildas Gonzalez <gildas.gonzalez@epitech.eu>
2021-06-08 12:17:01 +0200

    Master-B: Correcting CI on master

---
+++ build/CMakeFiles/unit_tests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unit_tests.dir/src/error/Error.cpp.o"
+  "CMakeFiles/unit_tests.dir/tests/tests_Error.cpp.o"
+  "unit_tests"
+  "unit_tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/unit_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
