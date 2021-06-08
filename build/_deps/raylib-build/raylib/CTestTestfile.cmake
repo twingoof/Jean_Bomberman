2ae7a59aebba1e997599b2a4005df0707be824ef
Gildas Gonzalez <gildas.gonzalez@epitech.eu>
2021-06-08 12:17:58 +0200

    Master-B: Removing build folder

--- build/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ggonzalez/epitech/tek2/b-yep/B-YEP-400-NAN-4-1-indiestudio-gildas.gonzalez/build/_deps/raylib-src/src
-# Build directory: /home/ggonzalez/epitech/tek2/b-yep/B-YEP-400-NAN-4-1-indiestudio-gildas.gonzalez/build/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
