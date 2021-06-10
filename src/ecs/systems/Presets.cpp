f4e0c4b8018bd0ed10f4e4f70459593adbd98a84
Gildas Gonzalez <gildas.gonzalez@epitech.eu>
2021-06-10 13:57:39 +0200

    Presets-FB: Correcting missing return on presets

--- src/ecs/systems/Presets.cpp
+++ src/ecs/systems/Presets.cpp
@@ -13,37 +13,42 @@
 #include "Moveable.hpp"
 #include "Drawable3D.hpp"
 
-ECS::Entity &Presets::createButton(std::string name, ECS::Vector3<float> position, void(callback)(), std::string spritePath)
+ECS::Entity Presets::createButton(std::string name, ECS::Vector3<float> position, void(callback)(), std::string spritePath)
 {
     ECS::Entity entity(name);
     ECS::Vector3<int> size(1, 1, 1);
     ECS::Transform t(position, size);
+    ECS::Clickable c(callback);
+    ECS::Drawable2D d(spritePath, t.getSize(), ECS::RECT);
 
     entity.addComponent<ECS::Transform>(t, ECS::TRANSFORM);
-    ECS::Clickable c(callback);
-    ECS::Drawable2D d(spritePath, entity.getComponent<ECS::Transform>(ECS::TRANSFORM).getSize(), ECS::RECT);
     entity.addComponent<ECS::Clickable>(c, ECS::CLICKABLE);
     entity.addComponent<ECS::Drawable2D>(d, ECS::DRAWABLE2D);
     return (entity);
 }
 
-ECS::Entity &Presets::createWall(std::string name, ECS::Vector3<float> position)
+ECS::Entity Presets::createWall(std::string name, ECS::Vector3<float> position)
 {
     ECS::Entity entity(name);
     ECS::Vector3<int> size(1, 1, 1);
     ECS::Transform t(position, size);
+    ECS::Drawable3D d(ECS::RECT, t.getSize());
 
     entity.addComponent<ECS::Transform>(t, ECS::TRANSFORM);
-    ECS::Drawable3D d(ECS::RECT, entity.getComponent<ECS::Transform>(ECS::TRANSFORM).getSize());
     entity.addComponent<ECS::Drawable3D>(d, ECS::DRAWABLE3D);
+    return (entity);
 }
 
-ECS::Entity &Presets::createPlayer(std::string name, ECS::Vector3<float> position)
+ECS::Entity Presets::createPlayer(std::string name, ECS::Vector3<float> position)
 {
     ECS::Entity entity(name);
     ECS::Vector3<int> size(1, 1, 1);
     ECS::Transform t(position, size);
+    ECS::Moveable m(t.getPosition());
+    ECS::Drawable3D d(ECS::RECT, t.getSize());
+    
     entity.addComponent<ECS::Transform>(t, ECS::TRANSFORM);
-    ECS::Moveable m(entity.getComponent<ECS::Transform>(ECS::TRANSFORM).getPosition());
-    ECS::Drawable3D d(ECS::RECT, entity.getComponent<ECS::Transform>(ECS::TRANSFORM).getSize());
+    entity.addComponent<ECS::Moveable>(m, ECS::MOVEABLE);
+    entity.addComponent<ECS::Drawable3D>(d, ECS::DRAWABLE3D);
+    return (entity);
 }
