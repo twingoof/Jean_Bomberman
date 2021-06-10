64c25c12dab553d7bcd33dff1293bf5f5861966d
Gildas Gonzalez <gildas.gonzalez@epitech.eu>
2021-06-10 11:43:59 +0200

    Factory-F: Adding Presets namespace

--- src/ecs/systems/Presets.cpp
+++ src/ecs/systems/Presets.cpp
@@ -12,7 +12,7 @@
 #include "Drawable2D.hpp"
 #include "Drawable3D.hpp"
 
-void createButton(ECS::Entity &entity, ECS::Vector3<float> position, ECS::Vector3<int> size, void(callback)(), std::string spritePath, ECS::DrawableType type)
+void Presets::createButton(ECS::Entity &entity, ECS::Vector3<float> position, ECS::Vector3<int> size, void(callback)(), std::string spritePath, ECS::DrawableType type)
 {
     ECS::Transform t(position, size);
     entity.addComponent<ECS::Transform>(t, ECS::TRANSFORM);
