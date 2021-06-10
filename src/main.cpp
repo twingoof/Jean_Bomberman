c0dab9b71aef003cff5de719addff30fc5e378e6
Pierrick PROST <prost.pierrick@outlook.fr>
2021-06-10 12:29:49 +0200

    Renderer-F: refactoring 3d drawing functions

--- src/main.cpp
+++ src/main.cpp
@@ -16,26 +16,29 @@
 #include "Controller.hpp"
 #include "Text.hpp"
 #include "Displacer.hpp"
-#include "MapGenerator.hpp"
+#include "Transform.hpp"
 
 int main()
 {
     raylib::Window &window = raylib::Window::getWindow();
-    raylib::Camera3D camera((Vector3){0, 70, 35}, (Vector3){0, 0, 0}, (Vector3){0, 1, 0}, 45, CAMERA_PERSPECTIVE);
+    raylib::Camera3D camera((Vector3){20, 20, 20}, (Vector3){0, 0, 0}, (Vector3){0, 1, 0}, 45, CAMERA_PERSPECTIVE);
     std::vector<ECS::Entity> mapEntities;
     ECS::Renderer r;
+    ECS::Entity e("boule");
+    ECS::Transform t({0.0f, 0.0f, 0.0f}, {10, 10, 10});
+    ECS::Drawable3D d(ECS::RECT, t.getSize());
+    std::vector<std::reference_wrapper<ECS::Entity>> v;
 
+    e.addComponent<ECS::Drawable3D>(d, ECS::DRAWABLE3D);
+    e.addComponent<ECS::Transform>(t, ECS::TRANSFORM);
+    v.push_back(e);
     window.initWindow(1000, 1000, "Bonjour Jeremy", FLAG_WINDOW_RESIZABLE);
-    man.createEntity("boule", ECS::Vector3<float> {0, 0, 0}, ECS::Vector3<int> {50, 50, 50});
-    man.addDrawable3D("boule", ECS::CIRCLE);
-    // man.createEntity("k-ré", {0, 0, 0}, {10, 10, 0});
-    // man.addDrawable3D("k-ré", ECS::DrawableType::RECT);
     while (!window.windowShouldClose())
     {
         window.beginDrawing();
         window.begin3DMode(camera);
         window.clearWindow(RAYWHITE);
-        r.draw(man.getEntities());
+        r.draw(v);
         DrawGrid(10, 1.0f);
         window.end3DMode();
         window.endDrawing();
