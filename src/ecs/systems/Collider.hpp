543cdf738d141fcf2ad9350663c811a19ce787d4
GildasGonz <gildas.gonzalez@epitech.eu>
2021-06-20 13:07:44 +0200

    Collider-D: Adding documentation on system Collider

--- src/ecs/systems/Collider.hpp
+++ src/ecs/systems/Collider.hpp
@@ -5,6 +5,16 @@
 ** Collider
 */
 
+/**
+ * @file Collider.hpp
+ * @author gildas.gonzalez@epitech.eu; pierrick.prost@epitech.eu; pierrick.prost@epitech.eu; valentin.bouchet@epitech.eu; mathis.ragot@epitech.eu
+ * @brief File that contain the Collider system
+ * @version 0.1
+ * @date 2021-06-20
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef COLLIDER_HPP_
 #define COLLIDER_HPP_
 
@@ -18,16 +28,52 @@
 #include <algorithm>
 #include <vector>
 
+/**
+ * @namespace ECS
+ */
 namespace ECS {
+    /**
+     * @class Collider Collider.hpp "src/ecs/systems/Collider.hpp"
+     */
     class Collider {
         public:
+            /**
+             * @fn 
+             * @brief Construct a new Collider object
+             */
             Collider() = default;
+
+            /**
+             * @fn 
+             * @brief Destroy the Collider object
+             */
             ~Collider() = default;
 
+            /**
+             * @fn void checkCollision(std::vector<ECS::Entity> &scene)
+             * @brief Check one by one all the entity collision
+             * 
+             * @param scene Entity of the game
+             */
             void checkCollision(std::vector<ECS::Entity> &scene);
 
         private:
+            /**
+             * @fn void isWindowColliding(ECS::Entity &entity)
+             * @brief Check is the entity is colliding with the window
+             * 
+             * @param entity Entity to check
+             */
             void isWindowColliding(ECS::Entity &entity);
+
+            /**
+             * @fn int isEntitesColliding(ECS::Entity &fEntity, ECS::Entity &sEntity)
+             * @brief Check if two entities are colliding
+             * 
+             * @param fEntity First entity
+             * @param sEntity Second entity
+             * @return int 
+             */
             int isEntitesColliding(ECS::Entity &fEntity, ECS::Entity &sEntity);
 
     };
