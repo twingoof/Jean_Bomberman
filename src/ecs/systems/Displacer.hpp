400ba6463480d0aed8669874039ce22e55518382
GildasGonz <gildas.gonzalez@epitech.eu>
2021-06-20 13:19:23 +0200

    Displacer-D: Adding displacer system

--- src/ecs/systems/Displacer.hpp
+++ src/ecs/systems/Displacer.hpp
@@ -5,6 +5,16 @@
 ** Displacer
 */
 
+/**
+ * @file Displacer.hpp
+ * @author gildas.gonzalez@epitech.eu; pierrick.prost@epitech.eu; pierrick.prost@epitech.eu; valentin.bouchet@epitech.eu; mathis.ragot@epitech.eu
+ * @brief File that contain the Displacer system
+ * @version 0.1
+ * @date 2021-06-20
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef DISPLACER_HPP_
 #define DISPLACER_HPP_
 
@@ -16,22 +26,62 @@
 #include <vector>
 #include <string>
 
+/**
+ * @namespace ECS
+ */
 namespace ECS {
+    /**
+     * @class Displacer Displacer.hpp "src/ecs/systems/Displacer.hpp"
+    */
     class Displacer {
         public:
+            /**
+             * @fn Displacer() = default
+             * @brief Construct a new Displacer object
+             */
             Displacer() = default;
+
+            /**
+             * @fn Displacer(const Displacer &oldDisplacer) = default
+             * @brief Construct a new Displacer object
+             * 
+             * @param oldDisplacer Old displacer to copy
+             */
             Displacer(const Displacer &oldDisplacer) = default;
+
+            /**
+             * @fn Displacer &operator=(const Displacer &oldDisplacer) = default
+             * @brief 
+             * 
+             * @param oldDisplacer Old displacer to copy
+             * @return Displacer& 
+             */
             Displacer &operator=(const Displacer &oldDisplacer) = default;
+
+            /**
+             * @fn ~Displacer() = default
+             * @brief Destroy the Displacer object
+             */
             ~Displacer() = default;
 
+
+            /**
+             * @fn void moveEntity(std::vector<ECS::Entity> &entities)
+             * @brief Move player
+             * 
+             * @param entities All the entities on the map
+             */
             void moveEntity(std::vector<ECS::Entity> &entities);
 
+
+            /**
+             * @fn void teleportEntity(ECS::Entity &entity, const ECS::Vector3<float> &positionVector)
+             * @brief Teleport player
+             * 
+             * @param entity Entity to move
+             * @param positionVector New position to teleport
+             */
             void teleportEntity(ECS::Entity &entity, const ECS::Vector3<float> &positionVector);
-
-        protected:
-
-        private:
-
     };
 };
 
